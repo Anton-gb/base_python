615095d9e4ac278a6c8545096a780e7864644800
Anton-gb <aoioho@mail.ru>
2021-09-26 23:17:26 +0300

    update

--- Timoshkov_Anton_dz_5_4.py
+++ Timoshkov_Anton_dz_5_4.py
@@ -1,5 +1,6 @@
 src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 
+# использовал range, а не enumerate, чтобы не затрагивать последний элемент списка
 res = [src[num + 1] for num in range(len(src) - 1) if src[num + 1] > src[num]]
 print(res)
 
--- Timoshkov_Anton_dz_5_5.py
+++ Timoshkov_Anton_dz_5_5.py
@@ -1,13 +1,13 @@
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 # result = [23, 1, 3, 10, 4, 11]
 
-uniq = set()
-_ts = set()
+unique = set()
+box = set()
 
 for i in src:
-    uniq.add(i) if i not in _ts else uniq.discard(i)
-    _ts.add(i)
+    unique.add(i) if i not in box else unique.discard(i)
+    box.add(i)
 
-result = [num for num in src if num in uniq]
+result = [num for num in src if num in unique]
 
 print(result)
