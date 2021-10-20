6834550aa8fc88cbc8d475ea3114f856203d3c30
Anton-gb <aoioho@mail.ru>
2021-10-20 20:29:55 +0300

    update

--- Timoshkov_Anton_dz_10_3.py
+++ Timoshkov_Anton_dz_10_3.py
@@ -2,26 +2,49 @@ class Cell:
     def __init__(self, num):
         self.num = num
         self.result = None
+        self.cells = None
 
     def __add__(self, other):
-        n = max(self.num, other.num)
-        # Клетка без пары
-        h = abs(self.num - other.num)
-        # Количество пар
-        self.result = n - h
-        # Количество ячеек
-        sum_cell = self.num + other.num
+        self.result = self.num + other.num
+        return Cell.make_order(self.result)
 
     def __sub__(self, other):
-        pass
+        self.result = abs(self.num - other.num)
+        if self.result > 0:
+            return Cell.make_order(self.result)
+        else:
+            return f"no"
 
-    def __floordiv__(self, other):
-        pass
+    def __mul__(self, other):
+        self.result = self.num * other.num
+        return Cell.make_order(self.result)
 
     def __truediv__(self, other):
-        pass
+        self.result = int(self.num / other.num)
+        return Cell.make_order(self.result)
+
+    @staticmethod
+    def make_order(cells):
+        s = ''
+        for i in range(cells):
+            s += '*'
+            if s.count('*') % 5 == 0:
+                s += '\n'
+        return s
 
 
 a = Cell(3)
 b = Cell(4)
+print('Сложение')
+res = a + b
+print(res)
+print('Вычитание')
+res = a - b
+print(res)
+print('Умножение')
+res = a * b
+print(res)
+print('Деление')
+res = a / b
+print(res)
 
